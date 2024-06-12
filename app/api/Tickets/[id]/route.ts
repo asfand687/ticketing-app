ef460052672f3e365010b35dee589157d2589c75
Asfand Yar <asfandyar687@gmail.com>
2024-06-12 14:35:34 +0500

    chore: added generics for param types

---
+++ app/api/Tickets/[id]/route.ts
@@ -0,0 +1,26 @@
+import Ticket from "@/app/(models)/ticket";
+import { NextResponse } from "next/server";
+
+// Define a generic type for params
+type Params<T> = {
+  params: T;
+};
+
+// Define the shape of the specific params for this function
+type DeleteParams = {
+  id: string;
+};
+
+export async function DELETE<T extends DeleteParams>({ params }: Params<T>) {
+
+  try {
+
+    const { id } = params
+    await Ticket.findByIdAndDelete(id)
+
+    return NextResponse.json({ message: "Ticket Deleted" }, { status: 200 })
+  } catch (error) {
+    console.error(error)
+    return NextResponse.json({ message: "Error", error}, { status: 500 })
+  }
+}
