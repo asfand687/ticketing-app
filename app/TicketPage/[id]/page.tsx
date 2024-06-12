c0aad9cc5d63833d17ad816842098e7930145ad2
Asfand Yar <asfandyar687@gmail.com>
2024-06-12 18:28:19 +0500

    chore: added edit mode functionality to form wrapper component

--- app/TicketPage/[id]/page.tsx
+++ app/TicketPage/[id]/page.tsx
@@ -1,10 +1,42 @@
 import TicketForm from '@/app/(components)/TicketForm'
 import React from 'react'
 
-const Ticket = ({ params }: { params: { id: number }}) => {
+const getTicketById = async(id: string) => {
+
+  const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+
+    cache: "no-store"
+  })
+  if(!res.ok) {
+
+    throw new Error("Failed to get ticket.")
+  }
+  
+  return res.json()
+}
+
+const Ticket = async ({ params }: { params: { id: string }}) => {
+
+  const EDIT_MODE = params.id === "new" ? false : true
+  let updateTicketData: Record<string, any> = {}
+
+  if(EDIT_MODE) {
+
+    updateTicketData = await getTicketById(params.id)
+    updateTicketData.ticket
+
+  } else {
+
+    updateTicketData = {
+      _id: "new"
+    }
+  }
+
   return (
     <div>
-      <TicketForm/>
+      <TicketForm
+        ticket={updateTicketData.ticket}
+      />
     </div>
   )
 }
