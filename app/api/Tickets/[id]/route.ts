c3b572235a4f460bc0374d04a32cdeb9248f62d2
Asfand Yar <asfandyar687@gmail.com>
2024-06-12 18:27:34 +0500

    feat: added update route to api

--- app/api/Tickets/[id]/route.ts
+++ app/api/Tickets/[id]/route.ts
@@ -11,6 +11,40 @@ type DeleteParams = {
   id: string;
 };
 
+export async function GET<T extends DeleteParams>(req: NextRequest, { params }: Params<T>) {
+ 
+  try {
+
+    const { id } = params
+    const ticket = await Ticket.findById(id)
+
+    return NextResponse.json({ ticket }, { status: 200 })
+  } catch (error) {
+    
+    console.error(error)
+    return NextResponse.json({ message: "Error", error}, { status: 500 })
+  }
+}
+
+export async function PUT<T extends DeleteParams>(req: NextRequest, { params }: Params<T>) {
+ 
+  try {
+
+    const { id } = params
+    const body = await req.json()
+
+    const ticket = await Ticket.findByIdAndUpdate(id, {
+      ...body.formData
+    })
+
+    return NextResponse.json({ message: "Ticket Updated" }, { status: 201 })
+  } catch (error) {
+    
+    console.error(error)
+    return NextResponse.json({ message: "Error", error}, { status: 500 })
+  }
+}
+
 export async function DELETE<T extends DeleteParams>(req: NextRequest, { params }: Params<T>) {
  
   try {
